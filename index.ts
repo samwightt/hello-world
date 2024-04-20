0a08a810f06c1ccc62db6fc128c0213377473280
Sam Wight <me@samw.dev>
2024-04-20 14:02:39 -0500

    Extract constant.

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { Message } from "./src/Message.ts";
 import { LoggerService } from "./src/LoggerService.ts";
+import { GREETING } from "./src/Constants.ts";
 
-const message = new Message("Hello, world!");
+const message = new Message(GREETING);
 const service = new LoggerService();
 service.logMessage(message);
