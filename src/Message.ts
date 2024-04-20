faf488714a2c358825fb46d3b1fa581f6dfa2757
Sam Wight <me@samw.dev>
2024-04-20 13:37:38 -0500

    Add docs to Message class.

--- src/Message.ts
+++ src/Message.ts
@@ -1,10 +1,25 @@
+/**
+ * Represents a message with content that can be get and set.
+ */
 export class Message {
+  /**
+   * Initializes a new instance of the Message class with the specified content.
+   * @param {string} content - The content of the message.
+   */
   constructor(private content: string) {}
 
+  /**
+   * Retrieves the content of the message.
+   * @returns {string} The current content of the message.
+   */
   getContent(): string {
     return this.content;
   }
 
+  /**
+   * Updates the content of the message.
+   * @param {string} newContent - The new content to set for the message.
+   */
   setContent(newContent: string): void {
     this.content = newContent;
   }
