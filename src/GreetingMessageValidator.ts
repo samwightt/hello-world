21de62bc33ef189ad95320b5067e93bd80cdc2b4
Sam Wight <me@samw.dev>
2024-04-20 14:09:10 -0500

    Add greeting docs.

--- src/GreetingMessageValidator.ts
+++ src/GreetingMessageValidator.ts
@@ -1,7 +1,15 @@
 import { Message } from "./Message.ts";
 import { GREETING } from "./Constants.ts";
 
+/**
+ * A validator class for greeting messages.
+ */
 export class GreetingMessageValidator {
+  /**
+   * Checks if a message is a valid greeting message.
+   * @param message The message to validate.
+   * @returns True if the message is a valid greeting message, false otherwise.
+   */
   static isValid(message: Message): boolean {
     return message.getContent() === GREETING;
   }
